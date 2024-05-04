1c695cf5b6523f14a403517ba763b4f389de818d
masroorali <masroor.ali@linentech.net>
2024-05-04 18:40:17 +0500

    fixed ui issue and added google tag manager

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,6 +37,17 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap"
           rel="stylesheet"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PP252CRX');
+      `,
+          }}
+        />
       </Head>
       <main className={`main app-${theme}`}>
         <Header theme={theme} toggleTheme={toggleTheme} isMobile={isMobile} />
