d572bda2d6a9845887ac69cdb3a9ff4f2df355f5
masroorali <masroor.ali@linentech.net>
2024-05-05 18:33:34 +0500

    added google api

--- src/pages/api/storeEmail.js
+++ src/pages/api/storeEmail.js
@@ -1,25 +1,28 @@
 import { google } from "googleapis";
+import {
+  GOOGLE_SHEETS_CLIENT_EMAIL,
+  GOOGLE_SHEETS_PRIVATE_KEY,
+  SPREADSHEET_ID,
+} from "./api_consts";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
       const { email } = req.body;
-
       const auth = new google.auth.GoogleAuth({
         credentials: {
-          client_email: "waheed.iqbal2030@gmail.com",
-          private_key: "b55395e0c6393285e920c001ce5886fee49688e5",
+          client_email: GOOGLE_SHEETS_CLIENT_EMAIL,
+          private_key: GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, "\n"),
         },
         scopes: ["https://www.googleapis.com/auth/spreadsheets"],
       });
 
       const sheets = google.sheets({ version: "v4", auth });
-      const spreadsheetId = "1VWSRvn6TiTsnWyCfzCZ47tQ94y2ZreYEZx14UvhNA0c";
-      const range = "Sheet1!A:A";
+      const spreadsheetId = SPREADSHEET_ID;
 
-      const response = sheets.spreadsheets.values.append({
+      const response = await sheets.spreadsheets.values.append({
         spreadsheetId,
-        range,
+        range: "Sheet1!A:A",
         valueInputOption: "USER_ENTERED",
         resource: {
           values: [[email]],
